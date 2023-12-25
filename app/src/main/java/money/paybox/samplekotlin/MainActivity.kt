0e42c4e64a296b87fb06568fe01ae4ba1d987d82
ZhuldyzAmankeldi <zhuldyz.amankeldi@freedompay.money>
2023-12-25 18:46:10 +0600

    added new method

--- app/src/main/java/money/paybox/samplekotlin/MainActivity.kt
+++ app/src/main/java/money/paybox/samplekotlin/MainActivity.kt
@@ -11,7 +11,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -53,6 +52,7 @@ class MainActivity : AppCompatActivity(), WebListener {
     private val email = "user@mail.com"
     private val phone = "77012345678"
     lateinit var url: String
+    val sdk by lazy { PayboxSdk.initialize(merchantId, secretKey) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -79,7 +79,7 @@ class MainActivity : AppCompatActivity(), WebListener {
                 .build()
         )
 
-        val sdk = PayboxSdk.initialize(merchantId, secretKey)
+      //  val sdk = PayboxSdk.initialize(merchantId, secretKey)
         sdk.setPaymentView(paymentView)
         paymentView.listener = this
         sdk.config().testMode(false)  //По умолчанию тестовый режим включен
@@ -392,7 +392,10 @@ class MainActivity : AppCompatActivity(), WebListener {
                             return
                         val paymentData = PaymentData.getFromIntent(data)
                         val token = paymentData?.paymentMethodToken?.token ?: return
-                        confirmGooglePayment(url, token)
+                        sdk.confirmGooglePayment(url, token) { payment, error ->
+                            Log.i("safasf","$payment")
+                        }
+                  //      confirmGooglePayment(url,token)
                     }
 
                     Activity.RESULT_CANCELED -> {
